907b41ae089123a44b244dc0eddb0c4f7f33b5ec
flamez of love <mechanicofthesequence@gmail.com>
2023-08-25 20:25:59 -0600

    remove accidentally committed test program

--- tests/index.js
+++
@@ -1,7 +0,0 @@
-import assert from "assert"
-import { parse } from "../build/debug.js"
-
-assert.deepEqual(parse("hello"), ["hello"])
-assert.deepEqual(parse("hello world"), ["hello", "world"])
-
-console.log("ok")
